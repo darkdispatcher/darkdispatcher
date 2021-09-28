be733116393f87f4786bc6317b801920e95df790
Michael Baird <mike@baird.rocks>
2021-09-28 10:01:10 -0500

    Added comments to IReadRepository

--- src/Core/Persistence/IReadRepository.cs
+++ src/Core/Persistence/IReadRepository.cs
@@ -9,14 +9,40 @@ namespace DarkDispatcher.Core.Persistence
 {
   public interface IReadRepository
   {
+    /// <summary>
+    /// Find a projection by id
+    /// </summary>
+    /// <param name="id">The id of the aggregate projection</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <typeparam name="TProjection">The implementation of <see cref="IProjection"/></typeparam>
+    /// <typeparam name="TId">The id of the aggregate projection.</typeparam>
+    /// <returns></returns>
     ValueTask<TProjection> FindAsync<TProjection, TId>(TId id, CancellationToken cancellationToken = default)
       where TProjection : class, IProjection
       where TId : AggregateId;
     
+    /// <summary>
+    /// Find a Projection by id and expression
+    /// </summary>
+    /// <param name="id">The id of the aggregate projection</param>
+    /// <param name="expression">The expression</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <typeparam name="TProjection">The implementation of <see cref="IProjection"/></typeparam>
+    /// <typeparam name="TId">The id of the aggregate projection.</typeparam>
+    /// <returns></returns>
     ValueTask<TProjection> FindAsync<TProjection, TId>(TId id, Expression<Func<TProjection, bool>> expression, CancellationToken cancellationToken = default) 
       where TProjection : class, IProjection
       where TId : AggregateId;
     
+    /// <summary>
+    /// Find all projections by id and expression
+    /// </summary>
+    /// <param name="id">The id of the aggregate projection</param>
+    /// <param name="expression">The expression</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <typeparam name="TProjection">The implementation of <see cref="IProjection"/></typeparam>
+    /// <typeparam name="TId">The id of the aggregate projection.</typeparam>
+    /// <returns></returns>
     ValueTask<IReadOnlyCollection<TProjection>> ListAsync<TProjection, TId>(TId id, Expression<Func<TProjection, bool>> expression, CancellationToken cancellationToken = default) 
       where TProjection : class, IProjection
       where TId : AggregateId;
